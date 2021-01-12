c9ce8e304897947414e94fffafef31db8d539fed
zsrozsii <zsadanyi.rozsa@gmail.com>
2021-01-12 16:44:32 +0100

    index.php edit

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>WebProg2 - Távoktatás 1</title>
+	<title>Oprendszer2 - Távoktatás</title>
 	<!-- Bootstrap CSS -->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 	<!-- Iconra hivatkozás -->
